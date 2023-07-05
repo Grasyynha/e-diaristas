dcb1ebd55cf3407e15c2bb7a0b407d0061500b1a
Grasiela Texeira <grasy.mst@gmail.com>
2023-07-05 15:12:13 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/treinaweb/ediaristas/core/repositories/ServicoRepository.java
@@ -0,0 +1,8 @@
+package br.com.treinaweb.ediaristas.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.treinaweb.ediaristas.core.models.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+    
+}

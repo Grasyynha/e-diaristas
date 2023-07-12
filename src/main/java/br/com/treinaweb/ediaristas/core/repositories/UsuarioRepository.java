e829f5c09aa322ceccf7f88920bd9494441cb1fa
Grasiela Texeira <grasy.mst@gmail.com>
2023-07-12 15:09:39 -0300

    Melhorando a Validação

--- src/main/java/br/com/treinaweb/ediaristas/core/repositories/UsuarioRepository.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/repositories/UsuarioRepository.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.ediaristas.core.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.treinaweb.ediaristas.core.models.Usuario;
 
@@ -11,5 +12,10 @@ import br.com.treinaweb.ediaristas.core.models.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional <Usuario> findByEmail(String email);
+
+    @Query("SELECT count(*) > 0 FROM Usuario u WHERE u.email = :email and (:id is null or u.id != :id)")
+    Boolean isEmailJaCadastrado(String email, Long id);
+
+
 
 }
